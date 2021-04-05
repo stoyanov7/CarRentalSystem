a6276362783dcda154b37ba200e1a1dbf771c46a
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-04-05 21:36:04 +0300

    create statistics models

---
+++ src/CarRentalSystem.Statistics.Data/Models/Statistics.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRentalSystem.Statistics.Data.Models
+{
+    public class Statistics
+    {
+        public int Id { get; set; }
+
+        public long TotalCarAds { get; set; }
+
+        public long TotalRentedCars { get; set; }
+    }
+}
