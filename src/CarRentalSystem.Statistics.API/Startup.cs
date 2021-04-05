a96d929c37558764ebaa0f79e0eef22a56e3ddf1
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-04-05 22:14:07 +0300

    configure statistic api to run

---
+++ src/CarRentalSystem.Statistics.API/Startup.cs
@@ -0,0 +1,31 @@
+namespace CarRentalSystem.Statistics.API
+{
+    using CarRentalSystem.Common.Configurations;
+    using CarRentalSystem.Statistics.API.Configurations;
+    using CarRentalSystem.Statistics.Data;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => this.Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        => services
+            .AddDatabase<StatisticsContext>(this.Configuration)
+            .AddApplicationSettings(this.Configuration)
+            .AddTokenAuthentication(this.Configuration)
+            .AddControllers();
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            => app
+                .UseWebService(env)
+                .UseDatabaseMigrate()
+                .UseSeedData();
+        
+    }
+}
