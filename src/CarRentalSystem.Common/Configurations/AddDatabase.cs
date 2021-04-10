8488ae3d7f6718d653eff9c9da80b3cf656f80d1
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-04-10 18:21:19 +0300

    configure database resilience

--- src/CarRentalSystem.Common/Configurations/AddDatabase.cs
+++ src/CarRentalSystem.Common/Configurations/AddDatabase.cs
@@ -3,6 +3,7 @@
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using System;
 
     public static partial class ConfigurationExtensions
     {
@@ -10,6 +11,14 @@
             where TDbContext : DbContext
             => services
                 .AddScoped<DbContext, TDbContext>()
-                .AddDbContextPool<TDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+                .AddDbContextPool<TDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        sqlServerOptionsAction => sqlServerOptionsAction.EnableRetryOnFailure(
+                            maxRetryCount: 10,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null)
+                        )
+                );
     }
 }
